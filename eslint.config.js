5c33393035c40f4287d1731969a42c0c52488980
bigbosssnakeifyart@gmail.com <bigbosssnakeify@gmail.com>
2024-12-19 05:09:05 +0200

    chore: refactor ESLint configuration to use FlatConfig format

--- eslint.config.js
+++ eslint.config.js
@@ -4,35 +4,31 @@ import pluginTs from "@typescript-eslint/eslint-plugin";
 import pluginReact from "eslint-plugin-react";
 import parser from "@typescript-eslint/parser";
 
-/** @type {import('eslint').Linter.Config} */
-export default {
-  files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-  languageOptions: {
-    globals: globals.browser,
-    parser: parser,
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+/** @type {import('eslint').Linter.FlatConfig} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
       },
-      ecmaVersion: 12,
-      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": pluginTs,
+      "react": pluginReact,
+    },
+    rules: {
+      // Add any custom rules here
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
-  plugins: [
-    "@typescript-eslint",
-    "react",
-  ],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-  ],
-  rules: {
-    // Add any custom rules here
-    "@typescript-eslint/no-unused-vars": "warn",
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-    "no-console": "warn",
-    "no-debugger": "warn"
-  },
-};
+  pluginJs.configs.recommended,
+  pluginTs.configs.recommended,
+  pluginReact.configs.recommended,
+];
